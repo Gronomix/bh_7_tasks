5cc07f5a6c2b3cc363639079fa1e50e69713f3b8
ГерманМА <Gronomix2013@gmail.com>
2021-03-16 09:51:57 +0300

    нужно доделать

--- easy/abstract_class/animals.py
+++ easy/abstract_class/animals.py
@@ -8,4 +8,34 @@
 
 На основе Animal определить классы Cat, Dog, Cow, которые переопределят метод says,
 чтобы он выводил, например "Кошка {name} говорит МЯУ"
+
 """
+
+from abc import ABC, abstractmethod
+class Animals(ABC):
+    name: str
+    a_type: str
+
+    def __init__(self, name, a_type):
+        self.name = name
+        self.a_type = a_type
+
+    @abstractmethod
+    def says(cls, name,):
+        cls.name = name
+
+
+
+
+class Cat(Animals):
+
+    print(f'Кошка {name} говорит Мяу')
+
+class Dog(Animals):
+        print(f'Собака {name} говорит ГАВ')
+
+class Cow(Animals):
+     print(f'Корова {name} говорит МУУУУ')
+
+
+
