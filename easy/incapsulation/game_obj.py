5709402c8111c0c613f022e85f36a07a09363b7e
ГерманМА <Gronomix2013@gmail.com>
2021-03-24 08:09:45 +0300

    непонятно как реализовать сеттер с 2 переменными

--- easy/incapsulation/game_obj.py
+++ easy/incapsulation/game_obj.py
@@ -9,3 +9,29 @@ move(delta_x, delta_y)
 
 реализовать через property
 """
+
+
+class GameObject:
+    _x: int
+    _y: int
+
+    def __init__(self, x=4, y=6):
+        self._x = x
+        self._y = y
+
+    @property
+    def move(self):
+        return self._x, self._y
+
+    @move.setter
+    def move(self, delta_x):
+        self._x = delta_x
+
+    @move.setter
+    def move(self, delta_y):
+        self._y = delta_y
+
+
+gameobj = GameObject
+print(gameobj.move)
+
