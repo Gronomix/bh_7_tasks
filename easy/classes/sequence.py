0b6b0108e6727456d467fe9494efc572b74791d7
Gronomix <Gronomix2013@gmail.com>
2021-03-25 19:20:33 +0300

    Решено

--- easy/classes/sequence.py
+++ easy/classes/sequence.py
@@ -16,6 +16,24 @@ import random
 # random.randint(-10, 10)
 
 class RandSequence:
+
+    sequence: list
+
+    def __init__(self, n: int):
+        self.generate(n)
+
+    def generate(self, n):
+        self.sequence = [random.randint(-10, 10) for i in range(n)]
+        return self.sequence
+
+    def print_seq(self):
+        print(self.sequence)
+
+
+rs = RandSequence(4)
+rs.print_seq()
+
+class RandSequence:
     sequence: list
 
     def __init__(self, n: int):
