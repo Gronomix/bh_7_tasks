31a507a1f4581f425061b865b3b095d2d187e507
Gronomix <Gronomix2013@gmail.com>
2021-03-25 18:53:49 +0300

    Решено

--- easy/incapsulation/laptop_game.py
+++ easy/incapsulation/laptop_game.py
@@ -12,3 +12,32 @@
   меньше, чем характеристики компьютера, то вывести
   "На данном ПК есть возможность играть в {game_name}"
 """
+
+class Laptop:
+
+    _cpu_cores: int
+    _gpu_total: int
+    _ram_total: int
+
+    def __init__(self, _cpu_cores, _gpu_total, _ram_total):
+        self._cpu_cores = _cpu_cores
+        self._gpu_total = _gpu_total
+        self._ram_total = _ram_total
+
+    def can_play(self, game_name):
+        if self._cpu_cores >= 2:
+            if self._gpu_total >= 4:
+                if self._ram_total >= 8:
+                    return f'На данном ПК есть возможность играть в {game_name}'
+                else:
+                    return f'Не жмись купи комп что бы играть в {game_name}'
+            else:
+                return f'Не жмись купи комп что бы играть в {game_name}'
+        else:
+            return f'Не жмись купи комп что бы играть в {game_name}'
+
+
+laptop1 = Laptop(2, 4, 9)
+laptop2 = Laptop(4, 8, 7)
+print(laptop1.can_play('CuberPank'))
+print(laptop2.can_play('CuberPank'))
