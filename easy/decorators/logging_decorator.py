457831a968f07ff9c9ad9618ec485d7aef573313
ГерманМА <Gronomix2013@gmail.com>
2021-03-29 15:06:13 +0300

    решено

--- easy/decorators/logging_decorator.py
+++ easy/decorators/logging_decorator.py
@@ -4,3 +4,38 @@
 "Выполняем {func.__name__} с args: {args} и kwargs: {kwargs}".
 После выполнения функции напечатать строку "Выполнено {func.__name__}"
 """
+
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f' Выполняем {func.__name__}; args {args}; kwargs {kwargs}')
+        result = func(*args, **kwargs)
+        print(f'Выполнено {func.__name__}')
+        return result
+    return wrapper
+
+
+def class_decor(cls):
+    call_attr = {k: v for k, v in cls.__dict__.items() if callable(v)}
+
+    for name, func in call_attr.items():
+        dec_func = decorator(func)
+        setattr(cls, name, dec_func)
+    return cls
+
+
+@class_decor
+class SomeClass:
+
+    def __init__(self, name):
+        self.name = name
+
+    def some_method(self):
+        print('hi')
+
+    def some_method1(self):
+        print(' Woy')
+
+sc = SomeClass(1)
+sc.some_method()
+sc.some_method1()
