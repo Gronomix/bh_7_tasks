411e51a23f7619195bc79c0eff2c85ab41bfcf32
ГерманМА <Gronomix2013@gmail.com>
2021-03-29 12:42:12 +0300

    решено

--- easy/incapsulation/game_obj.py
+++ easy/incapsulation/game_obj.py
@@ -15,23 +15,24 @@ class GameObject:
     _x: int
     _y: int
 
-    def __init__(self, x=4, y=6):
+    def __init__(self, x: int, y: int):
         self._x = x
         self._y = y
 
     @property
-    def move(self):
-        return self._x, self._y
+    def x(self):
+        return self.x
 
-    @move.setter
-    def move(self, delta_x):
+    @property
+    def y(self):
+        return self.y
+
+    def move(self, delta_x, delta_y):
         self._x = delta_x
-
-    @move.setter
-    def move(self, delta_y):
         self._y = delta_y
 
 
-gameobj = GameObject
-print(gameobj.move)
+gameobj = GameObject(3, 4)
+gameobj.move(5, 6)
+print(gameobj)
 
