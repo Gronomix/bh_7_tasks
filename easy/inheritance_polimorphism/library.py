180f57bae3d14549646189b037e31965e396cb0f
ГерманМА <Gronomix2013@gmail.com>
2021-03-29 16:18:00 +0300

    Нужно допилить

--- easy/inheritance_polimorphism/library.py
+++ easy/inheritance_polimorphism/library.py
@@ -21,3 +21,40 @@
   и "Петров В. В. вернул 4 книги". Если какой то книги нет, то выводится сообщение
   "Петров В. В. не брал Рассказы"
 """
+
+class Person:
+
+    fio: str
+    phone: int
+
+    def __init__(self, fio, phone):
+        self.fio = fio
+        self.phone = phone
+
+
+class LibraryReader(Person):
+
+    id: int
+    books: list
+
+    def __init__(self, fio, phone, id, books):
+        super(LibraryReader, self).__init__(fio, phone)
+
+        self.id = id
+        self.books = books
+
+    def take_book(self, books,  *args, **kwargs):
+        if len(books) > 3:
+            return f'{self.fio} вернул книги: {self.books}'
+        else:
+            return f'Петров В. В. вернул 4 книги'
+
+
+book_list = ['Энциклопедия', 'Приключения', 'Словарь']
+person = Person('Петров В.В.', 222875)
+lp = LibraryReader
+lp.books = book_list
+print(lp.books)
+
+lp.take_book(1, books=book_list)
+print(lp.take_book)
