b5f422ef558e61d0728fda11fd2a68a67dacafd2
Gronomix <Gronomix2013@gmail.com>
2021-03-29 19:14:48 +0300

    что то пошло не так

--- easy/inheritance_polimorphism/mro_2.py
+++ easy/inheritance_polimorphism/mro_2.py
@@ -14,3 +14,48 @@ process_doc, который будет печатать "Делаю копию: 
 Создать объект класса MFU. Потренироваться вызывать методы через super,
 через имя класса. Просмотреть MRO
 """
+
+
+class Device:
+
+    def process_doc(self):
+        raise NotImplementedError
+
+
+class Scanner(Device):
+
+    name: str
+
+    def __init__(self, name):
+        self.name = name
+
+    def process_doc(self, name):
+        print(f'Сканирую документ: {self.name}')
+
+
+class Copier(Device):
+    name: str
+
+    def __init__(self, name):
+
+        self.name = name
+
+    def process_doc(self, name):
+        super().process_doc()
+        print(f'делаю копию: {self.name}')
+
+
+class MFU(Scanner, Copier):
+    
+    def __init__(self, name):
+        super(MFU, self).__init__(name)
+    
+    def process_doc(self, name):
+        super().process_doc()
+        print(f'Сканирую, отправляю факс: {self.name}')
+
+
+mfu = MFU
+mfu.process_doc()
+
+
